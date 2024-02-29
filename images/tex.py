953e2834fc9942a76759bbcfa4875e605e0906bc
FiddyFive <32050099+FiddyFive@users.noreply.github.com>
2024-02-29 17:57:30 +1100

    Update tex.py
    Use the new formatting introduced in the Zachtronics Solitaire Collection

--- images/tex.py
+++ images/tex.py
@@ -7,25 +7,34 @@ import lz4.block
 import sys
 import os
 
+# https://gist.github.com/FiddyFive/ca6d110823f7dce31bc8d2884e9fdc0f
 
 class Tex:
-    SIGN = b'\xea\x03\x00\x00'
+    SIGN = b'\xed\x03\x00\x00'
 
     def __init__(self, read_io=None):
         if read_io is not None:
             self.load(read_io)
 
     def load(self, read_io):
+        # Read 4 bytes. Address now 0x04
         if read_io.read(len(self.SIGN)) != self.SIGN:
             raise TypeError('wrong file type')
+        
+        # Skip over 0x4 bytes of unneeded data. Address now 0x8.
+        self.unknownData = unpack("<I", read_io.read(4))
 
+        # Read 0xC (12) bytes. Address now 0x14.
         self.width, self.height, self.type = unpack("<3I", read_io.read(0xc))
-        self.dummy = read_io.read(0x28)
+        
+        # Skip over 0x34 bytes of unneeded data. Address now 0x48.
+        self.dummy = read_io.read(0x34)
         zsize, = unpack('<I', read_io.read(4))
         self.zbuf = read_io.read(zsize)
 
     def save(self, write_io):
         write_io.write(self.SIGN)
+        write_io.write(self.unknownData)
         write_io.write(pack("<3I", self.width, self.height, self.type))
         write_io.write(self.dummy)
         write_io.write(pack('<I', len(self.zbuf)))
